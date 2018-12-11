6b203efafe6f927a8f527ba0d3811cfb6d6af538
Jakob Nielsen <jakob.nielsen@dfds.com>
2018-12-11 16:11:06 +0100

    Added additional input validation and logging.

--- src/IAMRoleService.WebApi/Controllers/RoleController.cs
+++ src/IAMRoleService.WebApi/Controllers/RoleController.cs
@@ -1,10 +1,15 @@
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Amazon;
 using Amazon.IdentityManagement;
 using Amazon.IdentityManagement.Model;
 using Amazon.Runtime;
 using IAMRoleService.WebApi.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Serilog;
 
 namespace IAMRoleService.WebApi.Controllers
 {
@@ -23,9 +28,10 @@ namespace IAMRoleService.WebApi.Controllers
         [HttpPost("")]
         public async Task<IActionResult> Create([FromBody] CreateIAMRoleRequest input)
         {
-            if (string.IsNullOrWhiteSpace(input.Name))
+            if (TryValidateCreateRoleRequest(input, out string validationError))
             {
-                return BadRequest("Name is invalid.");
+                Log.Warning($"Create role called with invalid input. Validation error: {validationError}");
+                return BadRequest(validationError);
             }
 
             var request = new CreateRoleRequest
@@ -34,9 +40,47 @@ namespace IAMRoleService.WebApi.Controllers
                 AssumeRolePolicyDocument = @"{""Version"":""2012-10-17"",""Statement"":[{""Effect"":""Allow"",""Principal"":{""AWS"":""" + _accountArn + @"""},""Action"":""sts:AssumeRole"",""Condition"":{}}]}"
             };
 
-            await _client.CreateRoleAsync(request);
+            var response = await _client.CreateRoleAsync(request);
+
+            if (response.HttpStatusCode != HttpStatusCode.OK)
+            {
+                var metadata = string.Join(", ", response.ResponseMetadata.Metadata.Select(m => $"{m.Key}:{m.Value}"));
+                Log.Error($"Error creating role. Status code was {response.HttpStatusCode}, metadata: {metadata}");
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occured trying to create the IAM role.");
+            }
 
             return Ok();
+        }
+
+        private bool TryValidateCreateRoleRequest(CreateIAMRoleRequest request, out string validationError)
+        {
+            validationError = string.Empty;
+
+            if (string.IsNullOrWhiteSpace(request.Name))
+            {
+                validationError = "Name is invalid.";
+                return false;
+            }
+
+            // Role name 64 char max.
+            if (request.Name.Length > 64)
+            {
+                validationError = "Name is invalid. A maximum of 64 characters is allowed.";
+                return false;
+            }
+
+            // Only alphanumeric and '+=,.@-_' allowed.
+            var allowedCharactersPattern = "^[a-zA-Z0-9!@#$&()\\-`.+,/\"]*$";
+            var match = Regex.Match(request.Name, allowedCharactersPattern, RegexOptions.IgnoreCase);
+
+            if (!match.Success)
+            {
+                validationError = "Name is invalid. Only alphanumeric and '+=,.@-_' is allowed.";
+                return false;
+            }
+
+            return true;
         }
     }
 }
