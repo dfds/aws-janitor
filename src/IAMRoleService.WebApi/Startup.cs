3b5b150b4886506ccaa17a7ab6c433a0e513e84b
Kim Lindhard <kilin@dfds.com>
2018-12-11 12:42:55 +0100

    add swagger

--- src/IAMRoleService.WebApi/Startup.cs
+++ src/IAMRoleService.WebApi/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace IAMRoleService.WebApi
 {
@@ -27,7 +28,10 @@ namespace IAMRoleService.WebApi
                 )
             );
             
-            
+            services.AddSwaggerGen(c =>
+            {
+               c.SwaggerDoc("v1", new Info {Title = "Cognito API", Version = "v1"});
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +47,11 @@ namespace IAMRoleService.WebApi
             }
 
             app.UseMvc();
+            
+            app.UseSwagger();
+            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "IAM Role Service API"); });
+
+
         }
     }
 }
