9fc85e2f66c4b304db2f46852d0a3777745a5eb2
Kim Lindhard <kilin@dfds.com>
2019-01-18 10:25:08 +0100

    test(RoleFactory): add Will_Return_A_Role_If_CreateRoleAsync_Response_Is_Ok

--- src/IAMRoleService.WebApi.Tests/RoleFactoryFacts/CreateStsAssumableRoleAsyncFacts.cs
+++ src/IAMRoleService.WebApi.Tests/RoleFactoryFacts/CreateStsAssumableRoleAsyncFacts.cs
@@ -29,10 +29,41 @@ namespace IAMRoleService.WebApi.Tests.RoleFactoryFacts
                 amazonIdentityManagementServiceStubBuilder.WithCreateRoleResponse(createRoleResponse),
                 amazonSecurityTokenServiceStubBuilder.WithGetCallerIdentityResponse(getCallerIdentityResponse)
             );
-            // Act
 
+
+            // Act / Assert
             Assert.ThrowsAsync<Exception>(() => sut.CreateStsAssumableRoleAsync(roleName));
+        }
+
+
+        [Fact]
+        public async Task Will_Return_A_Role_If_CreateRoleAsync_Response_Is_Ok()
+        {
+            // Arrange
+            var role = new Role();
+            var createRoleResponse = new CreateRoleResponse();
+            createRoleResponse.HttpStatusCode = HttpStatusCode.OK;
+            createRoleResponse.Role = role;
+            var amazonIdentityManagementServiceStubBuilder = new AmazonIdentityManagementServiceStubBuilder();
+
+
+            var getCallerIdentityResponse = new GetCallerIdentityResponse();
+            getCallerIdentityResponse.Account = "AccountDoesNotMatter";
+            var amazonSecurityTokenServiceStubBuilder = new AmazonSecurityTokenServiceStubBuilder();
+            var roleName = "doesNotMatter";
+
+            var sut = new RoleFactory(
+                amazonIdentityManagementServiceStubBuilder.WithCreateRoleResponse(createRoleResponse),
+                amazonSecurityTokenServiceStubBuilder.WithGetCallerIdentityResponse(getCallerIdentityResponse)
+            );
+
+
+            // Act 
+            var resultRole = await sut.CreateStsAssumableRoleAsync(roleName);
+
+
             // Assert
+            Assert.Same(role, resultRole);
         }
     }
 }
