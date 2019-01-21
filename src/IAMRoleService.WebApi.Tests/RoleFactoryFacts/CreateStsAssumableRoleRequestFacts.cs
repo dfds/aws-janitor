14bce3f76d05987001cf4c34737e956fd1c350e0
Kim Lindhard <kilin@dfds.com>
2019-01-21 14:47:17 +0100

    fix test

--- src/IAMRoleService.WebApi.Tests/RoleFactoryFacts/CreateStsAssumableRoleRequestFacts.cs
+++ src/IAMRoleService.WebApi.Tests/RoleFactoryFacts/CreateStsAssumableRoleRequestFacts.cs
@@ -21,28 +21,9 @@ namespace IAMRoleService.WebApi.Tests.RoleFactoryFacts
             Assert.Equal(roleName, assumableRoleRequest.RoleName);
         }
 
-
         [Fact]
         //allowed or denied access to a resource. The
-        public void will_Allow_Access_for_Account()
-        {
-            var accountArn = new AwsAccountArn("foo");
-            var roleName = "baa";
-            var sut = new RoleFactory(null,null);
-
-
-            // Act
-            var assumableRoleRequest = sut.CreateStsAssumableRoleRequest(accountArn, roleName);
-
-
-            // Assert
-            var expectedSubstring = @"{""Effect"":""Allow"",""Principal"":{""AWS"":""" + accountArn + @"""}";
-            Assert.Contains(expectedSubstring, assumableRoleRequest.AssumeRolePolicyDocument);
-        }
-        
-        [Fact]
-        //allowed or denied access to a resource. The
-        public void Account_Amazon_Resource_Name_Will_Point_To_Root()
+        public void Principal_Will_Point_To_Federated_Login()
         {
             var accountArn = new AwsAccountArn("foo");
             var roleName = "baa";
