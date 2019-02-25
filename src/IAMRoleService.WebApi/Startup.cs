6a3fe06c829b6480641b03b0b9fbdd0ba12642b2
Thomas Fischer <thfis@dfds.com>
2019-02-25 09:03:24 +0100

    add health check and metrics

--- src/IAMRoleService.WebApi/Startup.cs
+++ src/IAMRoleService.WebApi/Startup.cs
@@ -1,15 +1,24 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using Amazon;
 using Amazon.IdentityManagement;
 using Amazon.SecurityToken;
 using IAMRoleService.WebApi.Infrastructure.Aws;
 using IAMRoleService.WebApi.Validators;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Hosting;
+using Prometheus;
 using Swashbuckle.AspNetCore.Swagger;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
 
 namespace IAMRoleService.WebApi
 {
@@ -51,6 +60,11 @@ namespace IAMRoleService.WebApi
             services.AddTransient<ICreateIAMRoleRequestValidator, CreateIAMRoleRequestValidator>();
             services.AddTransient<IRoleFactory, RoleFactory>();
             services.AddTransient<IParameterStore, ParameterStore>();
+
+            services.AddHostedService<MetricHostedService>();
+
+            services.AddHealthChecks()
+                .AddCheck("self", () => HealthCheckResult.Healthy());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -60,10 +74,11 @@ namespace IAMRoleService.WebApi
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
+
+            app.UseHealthChecks("/healthz", new HealthCheckOptions
             {
-                app.UseHsts();
-            }
+                ResponseWriter = MyPrometheusStuff.WriteResponseAsync
+            });
 
             app.UseMvc();
 
@@ -87,6 +102,80 @@ namespace IAMRoleService.WebApi
             });
 
             app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "IAM Role Service API"); });
+        }
+    }
+
+    public class MetricHostedService : IHostedService
+    {
+        private const string Host = "0.0.0.0";
+        private const int Port = 8080;
+
+        private IMetricServer _metricServer;
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine($"Staring metric server on {Host}:{Port}");
+
+            _metricServer = new KestrelMetricServer(Host, Port).Start();
+
+            return Task.CompletedTask;
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            using (_metricServer)
+            {
+                Console.WriteLine("Shutting down metric server");
+                await _metricServer.StopAsync();
+                Console.WriteLine("Done shutting down metric server");
+            }
+        }
+    }
+
+    public static class MyPrometheusStuff
+    {
+        private const string HealthCheckLabelServiceName = "service";
+        private const string HealthCheckLabelStatusName = "status";
+
+        private static readonly Gauge HealthChecksDuration;
+        private static readonly Gauge HealthChecksResult;
+
+        static MyPrometheusStuff()
+        {
+            HealthChecksResult = Metrics.CreateGauge("healthcheck",
+                "Shows health check status (status=unhealthy|degraded|healthy) 1 for triggered, otherwise 0", new GaugeConfiguration
+                {
+                    LabelNames = new[] { HealthCheckLabelServiceName, HealthCheckLabelStatusName },
+                    SuppressInitialValue = false
+                });
+
+            HealthChecksDuration = Metrics.CreateGauge("healthcheck_duration_seconds",
+                "Shows duration of the health check execution in seconds",
+                new GaugeConfiguration
+                {
+                    LabelNames = new[] { HealthCheckLabelServiceName },
+                    SuppressInitialValue = false
+                });
+        }
+
+        public static Task WriteResponseAsync(HttpContext httpContext, HealthReport healthReport)
+        {
+            UpdateMetrics(healthReport);
+
+            httpContext.Response.ContentType = "text/plain";
+            return httpContext.Response.WriteAsync(healthReport.Status.ToString());
+        }
+
+        private static void UpdateMetrics(HealthReport report)
+        {
+            foreach (var (key, value) in report.Entries)
+            {
+                HealthChecksResult.Labels(key, "healthy").Set(value.Status == HealthStatus.Healthy ? 1 : 0);
+                HealthChecksResult.Labels(key, "unhealthy").Set(value.Status == HealthStatus.Unhealthy ? 1 : 0);
+                HealthChecksResult.Labels(key, "degraded").Set(value.Status == HealthStatus.Degraded ? 1 : 0);
+
+                HealthChecksDuration.Labels(key).Set(value.Duration.TotalSeconds);
+            }
         }
     }
 }
