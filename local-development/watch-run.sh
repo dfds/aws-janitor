7b6d5ef5e270edf1c55d2e32d7a8a6637f8dd276
Kim Lindhard <kilin@dfds.com>
2019-03-28 11:44:02 +0100

    tweets for localdevelopment

--- local-development/watch-run.sh
+++ local-development/watch-run.sh
@@ -3,4 +3,5 @@
 KAFKA_BOOTSTRAP_SERVERS=localhost:9092 \
 KAFKA_GROUP_ID=kubernetes-consumer \
 KAFKA_ENABLE_AUTO_COMMIT=false \
+KUBERNETES_CLUSTER_NAME=hellman \
 dotnet watch --project ./../src/AwsJanitor.WebApi/AwsJanitor.WebApi.csproj run
