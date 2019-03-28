5840aff75863a2260b945333634f9d237e03802b
Kim Lindhard <kilin@dfds.com>
2019-03-28 09:51:23 +0100

    add policy model

---
+++ src/AwsJanitor.WebApi/Features/Roles/Model/Policy.cs
@@ -0,0 +1,26 @@
+namespace AwsJanitor.WebApi.Features.Roles.Model
+{
+    public class Policy
+    {
+        private Policy(string name, string document)
+        {
+            Name = name;
+            Document = document;
+        }
+        
+        public string Name { get; }
+        public string Document { get; }
+
+        
+        public static Policy Create(PolicyTemplate policyTemplate, string capabilityName)
+        {
+            var policy = new Policy(
+                name: policyTemplate.Name, 
+                document: policyTemplate.Document.Replace("capabilityName", capabilityName)
+            );
+
+            
+            return policy;
+        }
+    }
+}
