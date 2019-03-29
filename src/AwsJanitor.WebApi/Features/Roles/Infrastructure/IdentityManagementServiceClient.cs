62468653097965d5b63f24b3c0d38fedb0da523c
Kim Lindhard <kilin@dfds.com>
2019-03-29 14:30:55 +0100

    IdentityManagementServiceClient PutPoliciesAsync uses multiple threads

--- src/AwsJanitor.WebApi/Features/Roles/Infrastructure/IdentityManagementServiceClient.cs
+++ src/AwsJanitor.WebApi/Features/Roles/Infrastructure/IdentityManagementServiceClient.cs
@@ -32,70 +32,54 @@ namespace AwsJanitor.WebApi.Features.Roles.Infrastructure.Persistence
         {
             var policiesResponse =
                 await _client.ListRolePoliciesAsync(new ListRolePoliciesRequest {RoleName = roleName});
-            foreach (var policyName in policiesResponse.PolicyNames)
-            {
-                await _client.DeleteRolePolicyAsync(new DeleteRolePolicyRequest
-                {
-                    RoleName = roleName,
-                    PolicyName = policyName
-                });
-            }
+
+            await DeleteRolePoliciesAsync(roleName, policiesResponse.PolicyNames);
 
             await _client.DeleteRoleAsync(new DeleteRoleRequest {RoleName = roleName});
         }
-//        var tasks = new List<Task>();
-//            foreach (var policy in policyTemplates)
-//        {
-//            tasks.Add(Task.Run(async () =>
-//                {
-//                    var policyDocumentWithRoleName = policy.Document.Replace("capabilityName", roleName);
-//                    var rolePolicyRequest = new PutRolePolicyRequest
-//                    {
-//                        RoleName = roleName,
-//                        PolicyName = policy.Name,
-//                        PolicyDocument = policyDocumentWithRoleName
-//                    };
-//                    await _client.PutRolePolicyAsync(rolePolicyRequest);
-//                })
-//            );
-//        }
-//        Task.WaitAll(tasks.ToArray());
+
+        
         public async Task<IEnumerable<Policy>> PutPoliciesAsync(RoleName roleName, IEnumerable<Policy> policies)
         {
             var policiesAdded = new List<Policy>();
+            var tasks = new List<Task>();
             foreach (var policy in policies)
             {
-                GetRolePolicyResponse getRolePolicyResponse;
-                try
-                {
-                    getRolePolicyResponse = await _client.GetPolicyAsync(new GetRolePolicyRequest
-                        {RoleName = roleName, PolicyName = policy.Name});
+                tasks.Add(Task.Run(async () =>
+                        {
+                            GetRolePolicyResponse getRolePolicyResponse;
+                            try
+                            {
+                                getRolePolicyResponse = await _client.GetPolicyAsync(new GetRolePolicyRequest
+                                    {RoleName = roleName, PolicyName = policy.Name});
+                            }
+                            catch (NoSuchEntityException)
+                            {
+                                getRolePolicyResponse = null;
+                            }
 
-                }
-                catch (NoSuchEntityException)
-                {
-                    getRolePolicyResponse = null;
-                }
-                
-                if (
-                    getRolePolicyResponse != null &&
-                    policy.Document == Uri.UnescapeDataString(getRolePolicyResponse.PolicyDocument)
-                )
-                {
-                    continue;
-                }
-                
-                policiesAdded.Add(policy);
+                            if (
+                                getRolePolicyResponse != null &&
+                                policy.Document == Uri.UnescapeDataString(getRolePolicyResponse.PolicyDocument)
+                            )
+                            {
+                                return;
+                            }
 
-                var rolePolicyRequest = new PutRolePolicyRequest
-                {
-                    RoleName = roleName,
-                    PolicyName = policy.Name,
-                    PolicyDocument = policy.Document
-                };
-                await _client.PutRolePolicyAsync(rolePolicyRequest);
+                            policiesAdded.Add(policy);
+
+                            var rolePolicyRequest = new PutRolePolicyRequest
+                            {
+                                RoleName = roleName,
+                                PolicyName = policy.Name,
+                                PolicyDocument = policy.Document
+                            };
+                            await _client.PutRolePolicyAsync(rolePolicyRequest);
+                        }
+                    )
+                );
             }
-
+            Task.WaitAll(tasks.ToArray());
             return policiesAdded;
         }
 
